c915cc2dc3d784d8a9b81e6949c098d5fbc06160
leofyada <leofyada@gmail.com>
2025-08-25 20:55:02 -0300

    Inclui log no processo

--- code/importacao.py
+++ code/importacao.py
@@ -1,15 +1,13 @@
 # Importa biblioteca pandas
 import pandas as pd
 import openpyxl
-import logging
 
 # Função para realizar a importação dos arquivos
 def converte_parquet(caminho_arquivo, caminho_destino):
     
     # Importa arquivos .xlsx
     arquivo_excel = pd.read_excel(caminho_arquivo)
-    logging.info(f"Realizando a leitura do {caminho_arquivo}")
+ 
     # Retorna a base de dados em .parquet
     arquivo_excel.to_parquet(caminho_destino, engine="pyarrow", index=False)
-    logging.info(f"Convertendo para parquet")
 
--- code/main.py
+++ code/main.py
@@ -1,5 +1,6 @@
 from importacao import converte_parquet
 from transformacao import limpeza_basica
+import logging
 
 # Caminho para o arquivo com o baseline
 caminho_baseline = "/Users/leonardoyada/Desktop/megaedu-transformacao/data/bronze/20250825_baseline_escolas.xlsx"
@@ -14,14 +15,31 @@ caminho_destino_fonteunica = "/Users/leonardoyada/Desktop/megaedu-transformacao/
 # Caminho de destino da base limpa
 caminho_destino_limpa = "/Users/leonardoyada/Desktop/megaedu-transformacao/data/prata/base_limpa.parquet"
 
+# Configuração do log
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)sZ %(levelname)s %(name)s %(message)s",
+    datefmt="%Y-%m-%dT%H:%M:%S",
+)
+logger = logging.getLogger("megaedu.transformacao")
+
 def run_etl():
     
-    # Converte arquivos Excel para .parquet
-    converte_parquet(caminho_baseline, caminho_destino_baseline)
-    converte_parquet(caminho_fonteunica, caminho_destino_fonteunica)
-    
-    # Limpa os arquivos e retorna uma base tratada
-    limpeza_basica(caminho_destino_baseline, caminho_destino_fonteunica, caminho_destino_limpa)
+    try:
+
+        # Converte arquivos Excel para .parquet
+        converte_parquet(caminho_baseline, caminho_destino_baseline)
+        logger.info("Conversão do arquivo baseline para parquet realizado com sucesso")
+        converte_parquet(caminho_fonteunica, caminho_destino_fonteunica)
+        logger.info("Conversão do arquivo fonte única para parquet realizada com sucesso")    
+
+        # Limpa os arquivos e retorna uma base tratada
+        limpeza_basica(caminho_destino_baseline, caminho_destino_fonteunica, caminho_destino_limpa)
+        logger.info("Limpeza dos arquivos realizada com sucesso")
+
+    except Exception as e:
+        logger.exception("ETL failed", extra={"status": "error"})
+        raise
 
 if __name__ == "__main__":
     run_etl()
