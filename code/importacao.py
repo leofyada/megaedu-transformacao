d37bb1f92ecad7df2c4a3798c56af4acceb8916c
leofyada <leofyada@gmail.com>
2025-08-25 22:25:54 -0300

    Altera a forma de leitura de Excel

--- code/importacao.py
+++ code/importacao.py
@@ -1,13 +1,18 @@
 # Importa biblioteca pandas
 import pandas as pd
 import openpyxl
+from io import StringIO
+from xlsx2csv import Xlsx2csv
 
 # Função para realizar a importação dos arquivos
 def converte_parquet(caminho_arquivo, caminho_destino):
     
     # Importa arquivos .xlsx
-    arquivo_excel = pd.read_excel(caminho_arquivo)
+    buffer = StringIO()
+    Xlsx2csv(caminho_arquivo, outputencoding="utf-8").convert(buffer)
+    buffer.seek(0)
+    df = pd.read_csv(buffer)
  
     # Retorna a base de dados em .parquet
-    arquivo_excel.to_parquet(caminho_destino, engine="pyarrow", index=False)
+    df.to_parquet(caminho_destino, engine="pyarrow", index=False)
 
