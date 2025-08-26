7668b794331c41d7d40d20878623ebb853251ba5
leofyada <leofyada@gmail.com>
2025-08-26 08:51:11 -0300

    Ajuste as datas do arquivo para manter histórico de atualização

--- code/config.py
+++ code/config.py
@@ -1,14 +1,17 @@
 # Importa bibliotecas
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from pathlib import Path
-from datetime import date
+from datetime import datetime
+
+now = datetime.now()
 
 class Settings(BaseSettings):
     
     DATA_DIR: Path = Path.cwd() / "data"
     BRONZE_DIR: Path = DATA_DIR / "bronze"
     PRATA_DIR: Path = DATA_DIR / "prata"
-    RUN_DATE: str = date.today().strftime("%Y%m%d")
+    OURO_DIR: Path = DATA_DIR / "ouro"
+    RUN_DATE: str = now.strftime("%Y%m")
 
     BASELINE_XLSX: str = "{run}_baseline_escolas.xlsx"
     FONTEUNICA_XLSX: str = "{run}_fonte_unica.xlsx"
--- code/main.py
+++ code/main.py
@@ -1,5 +1,5 @@
 from importacao import converte_parquet
-from transformacao import limpeza_basica
+from transformacao import recursos
 from config import settings
 import logging
 
@@ -16,13 +16,14 @@ def run_etl():
     run = settings.RUN_DATE
     bronze = settings.BRONZE_DIR
     prata = settings.PRATA_DIR
+    ouro = settings.OURO_DIR
 
     caminho_baseline_xlsx = bronze / settings.BASELINE_XLSX.format(run=run)
     caminho_fonteunica_xlsx = bronze / settings.FONTEUNICA_XLSX.format(run=run)
 
     caminho_baseline_parquet = prata / f"{run}_baseline_escolas.parquet"
     caminho_fonteunica_parquet = prata / f"{run}_fonte_unica.parquet"
-    caminho_limpa_parquet = prata / f"{run}_base_limpa.parquet"
+    caminho_limpa_parquet = ouro / f"{run}_base_limpa.parquet"
 
     try:
 
@@ -33,8 +34,8 @@ def run_etl():
         logger.info("Conversão do arquivo fonte única para parquet realizada com sucesso")    
 
         # Limpa os arquivos e retorna uma base tratada
-        limpeza_basica(caminho_baseline_parquet, caminho_fonteunica_parquet, caminho_limpa_parquet)
-        logger.info("Limpeza dos arquivos realizada com sucesso")
+        recursos(caminho_baseline_parquet, caminho_fonteunica_parquet, caminho_limpa_parquet)
+        logger.info("Inclusão de variáveis de recursos/políticas realizada com sucesso")
 
     except Exception as e:
         logger.exception("ETL failed", extra={"status": "error"})
