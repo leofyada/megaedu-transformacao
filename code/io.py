be85443c0ad5129770b86e4827c15edf47d35947
leofyada <leofyada@gmail.com>
2025-08-27 20:59:27 -0300

    Inclui script para io

---
+++ code/io.py
@@ -0,0 +1,20 @@
+# Bibliotecas
+import pandas as pd
+from pathlib import Path
+
+# Pathish pode ser uma string ou um objeto Path
+Pathish = Union[str, Path]
+
+# Função que lê arquivo Excel
+def read_excel(path: Pathish) -> pd.DataFrame:
+    return pd.read_excel(path)
+
+# Função que lê arquivo parquet
+def read_parquet(path: Pathish) -> pd.DataFrame:
+    return pd.read_parquet(path, engine="pyarrow")
+
+# Função que escreve arquivo parquet
+def write_parquet(df: pd.DataFrame, path: Pathish) -> None:
+    Path(path).parent.mkdir(parents=True, exist_ok=True)
+    df.to_parquet(path, engine="pyarrow", index=False)
+
